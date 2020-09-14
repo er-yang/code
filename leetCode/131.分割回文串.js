5bd0b567dec84877ef9c58058fd0d29770421820
杨远财 <2205666018y@gmail.com>
2020-09-14 15:37:06 +0800

    partition string

--- leetCode/131.分割回文串.js
+++ leetCode/131.分割回文串.js
@@ -9,8 +9,38 @@
  * @param {string} s
  * @return {string[][]}
  */
-var partition = function(s) {
+var partition = function (s) {
+  let result = [];
+  let arr = s.split('');
+  if (!s) {
+    return [[]];
+  }
+  if (s.length === 1) {
+    return [[s]];
+  }
+  for (let i = 1; i <= arr.length; i++) {
+    if (!helper(arr.slice(0, i))) {
+      continue;
+    } else {
+      let branch = partition(s.substring(i));
+      branch.map((item) => item.unshift(s.substring(0, i)));
+      result = result.concat(branch);
+      // console.log(result);
+    }
+  }
 
+  return result;
+};
+var helper = (arr) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    if (arr[start] !== arr[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
 };
 // @lc code=end
-
