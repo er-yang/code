d3d6c67c082c6d5a0c576a18ea97e4edd7884624
杨远财 <2205666018y@gmail.com>
2021-03-02 11:44:33 +0800

    leetcode jump game

--- leetCode/45.跳跃游戏-ii.js
+++ leetCode/45.跳跃游戏-ii.js
@@ -30,6 +30,21 @@ var jump = function(nums) {
   }
   return count;
 };
+// 方法二 贪心算法，每次挑选最大的进行跳, 
+var jump = function(nums) {
+  let max = 0;
+  let step = 0;
+  let end = 0;  // 边界，当到达了边界，直接跳转到边界内能够跳转的最大值
+  for (let i = 0; i < nums.length - 1; i++) {
+    const num = nums[i];
+    max = Math.max(max, num + i);
+    if(i === end) {
+      end = max;
+      step++;
+    }
+  }
+  return step;
+}
 // console.log(jump([9,7,9,4,8,1,6,1,5,6,2,1,7,9,0]));
 // @lc code=end
 
--- leetCode/55.跳跃游戏.js
+++ leetCode/55.跳跃游戏.js
@@ -9,7 +9,7 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-// f(n) = f(i) || f(i-1)
+// f(n) = f(i) || f(i-1) 动态规划
 var canJump = function(nums) {
   let f = [true];
   for (let i = 1; i < nums.length; i++) {
@@ -27,5 +27,21 @@ var canJump = function(nums) {
   }
   return f[nums.length - 1];
 };
+
+// 计算最大跳跃长度
+var canJump = function(nums) {
+  let max = 1;
+  for (let i = 0; i < nums.length && i < max; i++) {
+    const num = nums[i];
+    let len = num + i + 1
+    if(max < len) {
+      max = len;
+    }
+    if(max >= nums.length) {
+      return true;
+    }
+  }
+  return false;
+}
 // @lc code=end
 
